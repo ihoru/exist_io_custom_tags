96d8c2f407c8f7843d43a6053a26c146dc0ad94a
ihoru <i.h.o.r.p.o.l.y.a.k.o.v@gmail.com>
2020-01-01 19:13:05 +0700

    in import_from_gdrive.sh log date/time execution start/finish

--- scripts/import_from_gdrive.sh
+++ scripts/import_from_gdrive.sh
@@ -3,5 +3,7 @@
 DIR=`dirname $0`;
 cd ${DIR}/..;
 source $PWD/ENV/bin/activate;
-
-python $PWD/import_from_gdrive.py >> logs/import_from_gdrive.log 2>&1;
+f=logs/import_from_gdrive.log;
+date >> $f;
+python $PWD/import_from_gdrive.py >> $f 2>&1;
+date >> $f;
